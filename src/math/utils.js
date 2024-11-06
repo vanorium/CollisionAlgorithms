1956aa74026fbe7c0cbf3d575296f349222578a4
vanorium <vanorium.dev@gmail.com>
2024-11-06 22:32:48 +0100

    typo in the clamp function fixed

--- src/math/utils.js
+++ src/math/utils.js
@@ -1,10 +1,10 @@
 export const utils = {
     clamp: (val, min, max) => {
-        return Math.max(Math.min(val, min), max)
+        return Math.max(min, Math.min(max, val))
     },
     modulo1: (max, i) => {
         if(i > max-1) return 0
         return i
     }
     
-} 
+}
